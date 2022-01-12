9660a557eec6f77e26d169d06d7f5ea700dd921a
jesflovel <46500478+jesflovel@users.noreply.github.com>
2022-01-11 19:36:35 -0600

    Add files via upload

---
+++ patrones de comportamiento/iterator.py
@@ -0,0 +1,100 @@
+from __future__ import annotations
+from collections.abc import Iterable, Iterator
+from typing import Any, List
+
+
+"""
+To create an iterator in Python, there are two abstract classes from the built-
+in `collections` module - Iterable,Iterator. We need to implement the
+`__iter__()` method in the iterated object (collection), and the `__next__ ()`
+method in theiterator.
+"""
+
+
+class AlphabeticalOrderIterator(Iterator):
+    '''
+    Se encargará de manejar las iteraciones
+    '''
+    """
+    Concrete Iterators implement various traversal algorithms. These classes
+    store the current traversal position at all times.
+    """
+
+    """
+    `_position` attribute stores the current traversal position. An iterator may
+    have a lot of other fields for storing iteration state, especially when it
+    is supposed to work with a particular kind of collection.
+    """
+    _position: int = None
+
+    """
+    This attribute indicates the traversal direction.
+    """
+    _reverse: bool = False
+#inicia las variables collection, reverse y position
+    def __init__(self, collection: WordsCollection, reverse: bool = False) -> None:
+        self._collection = collection
+        self._reverse = reverse
+        self._position = -1 if reverse else 0
+
+    def __next__(self):
+        """
+        The __next__() method must return the next item in the sequence. On
+        reaching the end, and in subsequent calls, it must raise StopIteration.
+        """
+        '''
+        next debe retornar el siguiente item consecuentemente, si es el ultimo
+        debe detener la iteración y para eso es utilizado el manejo de errores
+        try/catch 
+        '''
+        try:
+            value = self._collection[self._position]
+            self._position += -1 if self._reverse else 1
+        except IndexError:
+            raise StopIteration()
+
+        return value
+
+
+class WordsCollection(Iterable):
+    """
+    Concrete Collections provide one or several methods for retrieving fresh
+    iterator instances, compatible with the collection class.
+    """
+
+    def __init__(self, collection: List[Any] = []) -> None:
+        self._collection = collection
+
+    def __iter__(self) -> AlphabeticalOrderIterator:
+        """
+        The __iter__() method returns the iterator object itself, by default we
+        return the iterator in ascending order.
+        """
+        return AlphabeticalOrderIterator(self._collection)
+
+    def get_reverse_iterator(self) -> AlphabeticalOrderIterator:
+        return AlphabeticalOrderIterator(self._collection, True)
+
+    def add_item(self, item: Any): #agregará los elementos en la posción corresponiente
+        self._collection.append(item)
+
+
+if __name__ == "__main__":
+    # The client code may or may not know about the Concrete Iterator or
+    # Collection classes, depending on the level of indirection you want to keep
+    # in your program.
+    '''
+    Se agregarán a collection los items First, Second y Third
+    '''
+    collection = WordsCollection()
+    collection.add_item("First")
+    collection.add_item("Second")
+    collection.add_item("Third")
+
+    print("Straight traversal:")#imprimirá los elementos en orden ascendente
+    print("\n".join(collection))
+    print("")
+
+    print("Reverse traversal:") #Imrpimirá los elementos en orden descendente
+    print("\n".join(collection.get_reverse_iterator()), end="")
+    
